ed8f9dc261004649b08c97f1feec524db5f642da
mprogers <mprogers@mac.com>
2018-03-06 13:01:49 -0600

    Created models for flight, pilot, plane

---
+++ models/flight.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const FlightSchema = new mongoose.Schema({
+
+  _id: { type: Number, required: true },
+  name: {
+    type: String,
+    required: true,
+    default: 'Lexie'
+  },
+  startingPoint: {
+    type: String,
+    required: true,
+    default: 'KEVU'
+  },
+  endingPoint: {
+    type: String,
+    required: true,
+    default: 'KCPS'
+  },
+  durationHours: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  pilotId:{
+    type:Number,
+    required:true,
+  },
+  planeId:{
+    type:Number,
+    required:true,
+  }
+})
+module.exports = mongoose.model('Flight', FlightSchema)
